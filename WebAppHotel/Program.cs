cbe874f998efafca751b31453c72666b60f497a2
AHGesports <arshia.hemati@gmail.com>
2023-03-25 15:11:01 +0100

    security patch

--- WebAppHotel/Program.cs
+++ WebAppHotel/Program.cs
@@ -1,7 +1,12 @@
 using HotelAppClassLibrary.Databases;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Configuration
+.SetBasePath(Directory.GetCurrentDirectory())
+.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true)
+.AddUserSecrets<Program>()
+.AddEnvironmentVariables();
 // Add services to the container.
 builder.Services.AddRazorPages();
 
